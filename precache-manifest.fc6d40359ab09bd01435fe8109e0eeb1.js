a1ba544fea8090638a2b4ba2c0ae2df3395fb286
AlexyssAyala <00238918@uca.edu.sv>
2019-11-05 00:52:15 -0600

    Updates

---
+++ precache-manifest.fc6d40359ab09bd01435fe8109e0eeb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572894f73ee290ad6d01d58356ef194a",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "3361634b41c085cbd381",
+    "url": "/react1/static/css/main.5c439871.chunk.css"
+  },
+  {
+    "revision": "249fc7d307317e825419",
+    "url": "/react1/static/js/2.1d9bfc50.chunk.js"
+  },
+  {
+    "revision": "3361634b41c085cbd381",
+    "url": "/react1/static/js/main.d7d98f73.chunk.js"
+  },
+  {
+    "revision": "981c2ce9d21a96f10995",
+    "url": "/react1/static/js/runtime-main.31d29b8a.js"
+  }
+]);
